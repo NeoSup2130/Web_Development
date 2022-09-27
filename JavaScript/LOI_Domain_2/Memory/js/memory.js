b10ac37c947e81aa7897c9cf8bac3e11cf8c38f7
Slava <slavaxbox@hotmail.nl>
2022-09-27 10:08:02 +0200

    [Add] Memory index.html, added button to generate cards. [Add] memory.js, added Card class, yatesShuffle, view object and refactored code to use new Card class and Generate button.

--- JavaScript/LOI_Domain_2/Memory/js/memory.js
+++ JavaScript/LOI_Domain_2/Memory/js/memory.js
@@ -1,15 +1,57 @@
 "use strict";
 
-const myCardSet = [
+const myCardArray = [
     "duck", "kitten", "piglet", "puppy", "calf", "veal", "lamb", "rooster",
     "horse", "mouse", "dog", "cat", "goose", "goat", "sheep", "pig", "cow", "chick", "hen"
 ];
 
-const myField = document.getElementById('field');
-myField.addEventListener('click', onClickCard);
-window.addEventListener('load', populateField);
+let myCardSet = [];
 
-function onClickCard(event){
+class Card 
+{
+    constructor(card1, card2 = card1, set = card1, sound = card1) 
+    {
+        this.card1 = card1;
+        this.card2 = card2;
+        this.set = set;
+        this.sound = sound;
+    }
+}
+
+const arrayUtils = {
+    yatesShuffle : function(args) 
+    {
+        var m = args.length, i;
+    
+        while (m) 
+        {
+            i = Math.floor(Math.random() * m--);
+            let t = args[m];
+            args[m] = args[i];
+            args[i] = t;
+        }
+        return args;
+    }
+}
+
+const view = 
+{
+    myField : document.getElementById('field'),
+    btnGenerate : document.getElementById('generate'),
+    clearField : function() 
+    {
+        while(this.myField.firstChild) 
+        {
+            this.myField.removeChild(this.myField.firstChild);
+        }
+    }
+}
+
+view.myField.addEventListener('click', onClickCard);
+view.btnGenerate.addEventListener('click', populateField);
+
+function onClickCard(event)
+{
     console.log("myField: Card clicked!");
     if (event.target.hasAttribute('animal'))
     {
@@ -17,13 +59,18 @@ function onClickCard(event){
     }
 }
 
-function populateField(){
+function populateField()
+{
     console.log("Populate field!");
-    myCardSet.forEach(createCard);
+    view.clearField();
+    arrayUtils.yatesShuffle.call(this, myCardArray);
+    const myDblCardArray = myCardArray.concat(myCardArray);
+    myCardSet = myDblCardArray.map(animal => new Card(animal));
+    myCardSet.forEach(card => createCard(card.card1));
 }
 
-function createCard(value, index, array) {
-    // const halfLen = Math.round(array.length / 2);
+function createCard(value, index, array) 
+{
     let newTile = document.createElement('div');
     let newCard = document.createElement('img');
     const imagePath = "img/", exten = ".jpg";
@@ -31,6 +78,6 @@ function createCard(value, index, array) {
     newCard.setAttribute('src', cardImage);
     newCard.setAttribute('animal', value);
     
-    myField.appendChild(newTile);
+    view.myField.appendChild(newTile);
     newTile.appendChild(newCard);
 }
