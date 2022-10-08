ddf3f058d9ccf37a5b07bfcdf20215c324536ba3
Slava <slavaxbox@hotmail.nl>
2022-10-08 03:28:15 +0200

    [Add] Memory, index added new elements for the game timer, score, attempts and successes. [Add Memory, index added new divs to contain newly added elements. [Add] Memory, css added styles for game timer, flex container for game info (gameBar). [Refactor] Memory, js moved utility functions into utitilty namespace. [Refactor] Memory, js onClickCard has been abstracted with view object functions. [Add] Memory, js view object has new html elements (score, attempts, gameTimer etc). [Add] Memory, js view object has new functions to handle card rendering and other game related ones. [Add] Memory, js added dedicated player controller object which helps with game logic. [Add] Memory, js added greetPlayer function which asks for player name if there is no cookie for it and shows high scores. [Add] Memory, js added a game timer which works with a closure function.

--- JavaScript/LOI_Domain_2/Memory/js/memory.js
+++ JavaScript/LOI_Domain_2/Memory/js/memory.js
@@ -7,21 +7,11 @@ class Card
         this.card1 = cardObject.card1;
         this.card2 = cardObject.card2;
         this.set = cardObject.set;
-        this.sound = cardObject.sound;
+        this.sound = cardObject.card1 + ".wav";
     }
 }
 
-function fetchCards() 
-{
-    const path = './assets/cards.json';
-    return fetch(path).then
-        (response => response.json()).then
-            (cards => cards.map(card => new Card(card)));
-}
-
-let myCardArray = fetchCards().then(result => myCardArray = result);
-
-const arrayUtils = {
+const utilities = {
     yatesShuffle : function(args) 
     {
         var m = args.length, i;
@@ -34,15 +24,67 @@ const arrayUtils = {
             args[i] = t;
         }
         return args;
+    },
+
+    removeAllChildren(target) 
+    {
+        while(target.firstChild) 
+        {
+            target.removeChild(target.firstChild);
+        }
+    },
+
+    getCookie(key) 
+    {
+        return document.cookie.split('; ').find((row) => row.startsWith(`${key}=`))?.split('=')[1];
+    },
+    
+    setCookie(key, value, days)
+    {
+        const expire = new Date();
+        expire.setTime(expire.getTime() + days * 24 * 60 * 60 * 1000);
+        const newNameCookie = `${key}=${value};expires=${expire.toUTCString()};`;
+        console.log(newNameCookie + " current data: " + new Date().toUTCString());
+        document.cookie = newNameCookie;
+    },
+
+    updateElement(target, value) 
+    {
+        target.innerText = value;
+    },
+
+    // Returns undefined if it's not a number
+    getIntFromElement(target) 
+    {
+        return Number(target.innerText);
     }
 }
+
+function fetchCards() 
+{
+    const path = './assets/cards.json';
+    return fetch(path).then
+        (response => response.json()).then
+            (cards => cards.map(card => new Card(card)));
+}
+
+let myCardArray = fetchCards().then(result => myCardArray = result);
+let myCardSet = [];
 
 const view = 
 {
     myField : document.getElementById('field'),
     btnGenerate : document.getElementById('generate'),
     boardSelect : document.getElementById('boardSelection'),
+    score : document.getElementById('score'),
+    attempts : document.getElementById('attempts'),
+    successes : document.getElementById('successes'),
+    gameTime : document.getElementById('gameTime'),
+    timer : document.getElementById('timer'),
+    maxGameTime : 60 * 2.0,
+    gameTimeLeft : 60 * 2.0,
     boardSizes: ["board4", "board5", "board6"],
+    cardSetsLeft : 0,
 
     getBoardIndex : function()
     {
@@ -53,14 +95,168 @@ const view =
         const index = this.getBoardIndex();
         return this.boardSizes[index];
     },
-    clearField : function() 
+    restartGame : function() 
     {
-        while(this.myField.firstChild) 
+        utilities.removeAllChildren(this.myField);
+        utilities.updateElement(score, 0);
+        utilities.updateElement(attempts, 0);
+        utilities.updateElement(successes, 0);
+        this.gameTimeLeft = this.maxGameTime;
+        utilities.updateElement(this.gameTime, this.getGameTimeString());
+
+        playerController.gameOver = false;
+    },
+    isCardCovered : function(card) 
+    {
+        const targetClass = card.lastChild.classList;
+        return targetClass.contains('covered');
+    },
+    flipCard : function(card)
+    {
+        const targetClass = card.lastChild.classList;
+        targetClass.toggle("covered");
+        targetClass.toggle("uncovered");
+    },
+    getAnimalType : function(card)
+    {
+        const animalCard = card.firstChild;
+        if (animalCard.hasAttribute('animal'))
+            return animalCard.getAttribute('animal');
+        else return undefined;
+    },
+    playAnimalSound : (function()
+    {
+        let sound = new Audio(undefined);
+        function playSound(card) 
         {
-            this.myField.removeChild(this.myField.firstChild);
+            const index = Array.prototype.indexOf.call(card.parentNode.children, card);
+            sound.pause();
+            sound = new Audio("./assets/snd/" + myCardSet[index].sound);
+            sound.play();
         }
+        return playSound;
+    })(),
+
+    getGameTimeString()
+    {
+        function getValidNotation(i) {
+            return (i < 10) ? "0" + i : i;
+        }
+        const m = Math.floor(this.gameTimeLeft / 60);
+        const s = this.gameTimeLeft - m * 60;
+        return getValidNotation(m) + ':' + getValidNotation(s);
     }
 }
+
+const playerController = 
+{
+    selectedCards : [],
+    maxTimeOut : 1000,
+    allowMove : true,
+    gameOver : false,
+    scores : ['4x4', '5x5', '6x6'],
+    playerName : '',
+    addSelected : function(card) 
+    {
+        this.selectedCards.push(card);
+        if (this.selectedCards.length == 2)
+        {
+            this.resolveMatch(this.selectedCards);
+            this.selectedCards = [];
+            this.allowMove = false;
+        }
+    },
+    
+    resolveMatch : function(cards) 
+    {
+        let score = utilities.getIntFromElement(view.score);
+
+        let attempts = utilities.getIntFromElement(view.attempts);
+        utilities.updateElement(view.attempts, ++attempts);
+
+        const doMatch = view.getAnimalType(cards[0]) == view.getAnimalType(cards[1]);
+        if (doMatch)
+        {
+            let successes = utilities.getIntFromElement(view.successes);
+            utilities.updateElement(view.successes, ++successes);
+
+            utilities.updateElement(view.score, score + 50);
+            setTimeout(this.correctMatch.bind(this), this.maxTimeOut, cards);
+        }
+        else 
+        {
+            score -= 5;
+            utilities.updateElement(view.score, (score < 0) ? 0 : score);
+            setTimeout(this.wrongMatch.bind(this), this.maxTimeOut, cards);
+        }        
+    },
+
+    resolveNextMove : function(matched)
+    {
+        if (matched && --view.cardSetsLeft == 0) 
+        {
+            handleTimer.stop();
+            this.resolveWin();
+        }
+        this.allowMove = true;
+    },
+
+    correctMatch(cards) 
+    {
+        cards.forEach(target => utilities.removeAllChildren(target));
+        this.resolveNextMove(true);
+    },
+
+    wrongMatch(cards)
+    {
+        cards.forEach(target => view.flipCard(target));
+        this.resolveNextMove(false);
+    },
+
+    resolveWin()
+    {
+        const startTxt = `Congratulations ${this.playerName}! You have won!`;
+        const scoreKey = this.scores[view.getBoardIndex()];
+        let lastScore = utilities.getCookie(scoreKey);
+        // Als lastScore undefined is (false) dan willen we 0 ipv undefined.
+        lastScore = lastScore ? lastScore : 0;
+        const score = view.score.innerText;
+
+        if (Number(lastScore) < Number(score))
+        {
+            alert(startTxt + `\nYou have a new highscore for ${scoreKey} board!\nYour last score was ${lastScore}. New highscore is ${score}!`);
+            utilities.setCookie(scoreKey, score, 1);
+        } else {
+            alert(startTxt + `\nYou haven't reached a new highscore for ${scoreKey} board!\nYour last score was ${lastScore}. Your score is ${score}!`);
+        }
+    },
+    timeOut()
+    {
+        alert('Time has run out!');
+        this.gameOver = true;
+    }
+}
+
+function greetPlayer()
+{
+    let value = utilities.getCookie('playerName');
+    if (value == undefined)
+    {
+        value = encodeURIComponent(prompt("Wat is jouw naam nieuwe speler?"));
+        utilities.setCookie('playerName', value, 1);
+    }
+    let scores = '';
+    playerController.scores.forEach(size => 
+        {
+            const score = utilities.getCookie(size);
+            if (score != undefined)
+                scores += `board ${size} = ${score}\n`;
+        })
+    alert(`Welcome ${value}! You can play my memory game now :)\nYour last scores were\n` + scores);
+    playerController.playerName = value;
+}
+
+greetPlayer();
 
 view.myField.addEventListener('click', onClickCard);
 view.btnGenerate.addEventListener('click', startGame);
@@ -73,42 +269,74 @@ function onSelectFieldSize()
 
 function startGame()
 {
-    let myCardSet = [];
-    arrayUtils.yatesShuffle.call(this, myCardArray);
+    handleTimer.stop();
+    utilities.yatesShuffle.call(this, myCardArray);
 
     // Bereken de helft van de set. Ex. 5^2 = 25 dus de helft is 25 / 2 = 12
     const arrayEnd = Math.floor(Math.pow(4 + view.getBoardIndex(), 2) / 2);
 
+    view.cardSetsLeft = arrayEnd;
+    view.timer.style.visibility = 'visible';
+
     myCardSet = myCardArray.slice(0, arrayEnd);
     myCardSet = myCardSet.concat(myCardSet);
-    arrayUtils.yatesShuffle.call(this, myCardSet);
+    utilities.yatesShuffle.call(this, myCardSet);
 
     populateField(myCardSet);
+    handleTimer.start();
 }
+
+const handleTimer = (function()
+{
+    let timerID = 0;
+
+    function startTimer()
+    {
+        if (--view.gameTimeLeft > 0)
+        {
+            utilities.updateElement(view.gameTime, view.getGameTimeString());
+            timerID = setTimeout(startTimer, 1000);
+        } else {
+            utilities.updateElement(view.gameTime, '00:00');
+            setTimeout(playerController.timeOut.bind(playerController), 50);
+        } 
+    }
+
+    return {
+        start() {
+            startTimer();
+        },
+        getTimerID() {
+            return timerID;
+        },
+        stop() {
+            clearTimeout(timerID);
+        }
+    };
+})();
+
 
 function onClickCard(event)
 {
-    const targetClass = event.target.classList;
-
-    if (targetClass.contains("covered"))
-    {
-        targetClass.remove("covered");
-        targetClass.add("uncovered");
-    } 
+    const card = event.target.parentElement;
     
-    if (targetClass.contains('uncovered'))
+    if (view.isCardCovered(card) && playerController.allowMove && !playerController.gameOver)
     {
-        const animalCard = event.target.previousElementSibling;
-        if (animalCard.hasAttribute('animal'))
+        view.flipCard(card);
+        const animal = view.getAnimalType(card);
+
+        if (animal)
         {
-            console.log(animalCard.getAttribute('animal'));
+            console.log(animal);
+            view.playAnimalSound(card);
+            playerController.addSelected(card);
         }
-    }
+    } 
 }
 
 function populateField(cardDeck)
 {
-    view.clearField();
+    view.restartGame();
     cardDeck.forEach(card => createCard(card.card1));
 }
 
