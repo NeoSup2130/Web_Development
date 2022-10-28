137f668474eb0d26f744aa5376d10586759d921f
Slava <slavaxbox@hotmail.nl>
2022-10-28 19:56:17 +0200

    [Add] Timeline side project, added a small project to workout timeline feature.

---
+++ JavaScript/Projects/Timeline/main.js
@@ -0,0 +1,36 @@
+
+function getViewportSize()
+{
+    return {
+        width : (window.innerWidth || document.documentElement.clientWidth),
+        height : (window.innerHeight || document.documentElement.clientHeight)
+    };
+}
+
+function isElementInViewport(el) 
+{
+    const rect = el.getBoundingClientRect();
+    const viewportSize = getViewportSize();
+    return (rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= viewportSize.height &&
+            rect.right <= viewportSize.width);
+}
+
+const items = document.querySelectorAll(".timeline li");
+
+function callbackFunc() 
+{
+    for (var i = 0; i < items.length; i++) 
+    {
+        if (isElementInViewport(items[i])) 
+            items[i].classList.add("in-view");
+    }
+}
+
+callbackFunc();
+setInterval(callbackFunc, 0.333);
+
+// window.addEventListener("load", callbackFunc);
+// window.addEventListener("resize", callbackFunc);
+// window.addEventListener("scroll", callbackFunc);
