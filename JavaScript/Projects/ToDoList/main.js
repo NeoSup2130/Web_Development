4fff32b4797139c6dc8bfdf4e622f89064f93057
Slava <slavaxbox@hotmail.nl>
2022-11-03 13:30:33 +0100

    [Add] Todo list project, added a model and view objects. [Add] Todo list project, added cookie saving/loading for tasks.

--- JavaScript/Projects/ToDoList/main.js
+++ JavaScript/Projects/ToDoList/main.js
@@ -1,7 +1,104 @@
+function getCookie(key) 
+{
+    return document.cookie.split('; ').find((row) => row.startsWith(`${key}=`))?.split('=')[1];
+}
+
+function setCookie(key, value, days)
+{
+    const expire = new Date();
+    expire.setTime(expire.getTime() + days * 24 * 60 * 60 * 1000);
+    const newNameCookie = `${key}=${value};expires=${expire.toUTCString()};`;
+    console.log(newNameCookie + " current data: " + new Date().toUTCString());
+    document.cookie = newNameCookie;
+}
+
+class taskItem {
+    constructor(name, checked = false)
+    {
+        this.name = name;
+        this.checked = checked;
+    }
+}
+
+const model = {
+    createTask : function(taskName, checked = false)
+    {
+        const id = this.tasks.length;
+        this.tasks.push(new taskItem(taskName, checked));
+        return id;
+    },
+    getTaskID : function(event)
+    {
+        const id = event.target.parentElement.taskID;
+        console.log(id);
+        return id;
+    },
+    updateChecked : function(event)
+    {
+        this.tasks[this.getTaskID(event)].checked = event.target.checked;
+        this.saveTasks();
+    },
+    deleteTask : function (event)
+    {
+        const id = this.getTaskID(event);
+        this.tasks.splice(id, 1);                                                                                                                                                           
+        this.saveTasks();
+        view.remove(event);
+    },
+    saveTasks : function()
+    {
+        setCookie(this.cookieKey, JSON.stringify(this.tasks), 1);
+    },
+    loadTasks : function()
+    {
+        const data = JSON.parse(getCookie(this.cookieKey));
+        for (let i = 0; i < data.length; i++)
+        {
+            const task = data[i];
+            const id = this.createTask(task.name, task.checked);
+            view.display(id);
+        }
+    },
+    cookieKey : "savedTasks",
+    tasks : []
+}
+
+const view = {
+    todoList : document.getElementById('todo-list'),
+    display : function(taskID) 
+    {
+        const task = model.tasks[taskID];
+
+        let newItem = document.createElement('li');
+        newItem.classList.add('task-container');
+        newItem.taskID = taskID;
+
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = task.checked;
+        checkbox.addEventListener('change', model.updateChecked.bind(model), false);
+        newItem.appendChild(checkbox);
+
+        let taskP = document.createElement('p');
+        taskP.innerText = task.name;
+        newItem.appendChild(taskP);
+
+        let delBtn = document.createElement('button');
+        delBtn.addEventListener('click', model.deleteTask.bind(model), false);
+        delBtn.innerText = "Delete";
+        newItem.appendChild(delBtn);
+
+        this.todoList.firstElementChild.appendChild(newItem);
+    },
+    remove : function(event)
+    {
+        this.todoList.firstElementChild.removeChild(event.target.parentElement);
+    }
+}
 
 const form = document.forms[0];
-const todoList = document.getElementById('todo-list');
-form.addEventListener('submit', callbackFunc, false);
+form.addEventListener('submit', submitCallback, false);
+document.addEventListener('DOMContentLoaded', model.loadTasks.bind(model));
 
 function setInputInvalid(element)
 {
@@ -42,33 +139,12 @@ function isInputValid()
     }
 }
 
-function createTask(taskName)
+function resetForm()
 {
-    let newItem = document.createElement('li');
-    newItem.classList.add('task-container');
-
-    let checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    newItem.appendChild(checkbox);
-
-    let taskP = document.createElement('p');
-    taskP.innerText = taskName;
-    newItem.appendChild(taskP);
-
-    let delBtn = document.createElement('button');
-    delBtn.addEventListener('click', deleteTask, false);
-    delBtn.innerText = "Delete";
-    newItem.appendChild(delBtn);
-
-    todoList.firstElementChild.appendChild(newItem);
+    form["task"].value = '';
 }
 
-function deleteTask(event)
-{
-    todoList.firstElementChild.removeChild(event.target.parentElement);
-}
-
-function callbackFunc(event)
+function submitCallback(event)
 {
     event.preventDefault();
     if (!isInputValid())
@@ -76,6 +152,8 @@ function callbackFunc(event)
         return;
     }
 
-    createTask(form["task"].value);
-    form["task"].value = '';
+    const id = model.createTask(form["task"].value);
+    view.display(id);
+    model.saveTasks();
+    resetForm();
 }
